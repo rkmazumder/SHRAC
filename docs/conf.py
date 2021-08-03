95edab2e22ea65572b6c25153a901e9cf5195484
rkmazumder <rkmazumder@gmail.com>
2021-08-03 12:51:15 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -178,7 +178,7 @@ latex_elements = {
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'ha',
+    (master_doc, 'Hurricane Risk Analysis',
      u'ha Documentation',
      [author], 1)
 ]
